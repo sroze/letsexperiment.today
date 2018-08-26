19a9d009551de3efdd130b8a5dd6201a818b491f
Samuel ROZE <samuel.roze@gmail.com>
2018-08-26 17:43:46 +0100

    Add some more "reporting queries"

--- reporting/queries.sql
+++ reporting/queries.sql
@@ -1,7 +1,17 @@
--- Experiments
-SELECT e.name, c.email
+-- Experiments overview
+SELECT 
+  e.uuid, 
+  e.name, 
+  (SELECT COUNT(*) FROM experiments_collaborators ec WHERE ec.experiment_uuid = e.uuid) as number_of_collaborators, 
+  (SELECT COUNT(*) FROM expected_outcome eo WHERE eo.experiment_uuid = e.uuid) as number_of_outcomes, 
+  (SELECT COUNT(*) FROM check_in ci WHERE ci.experiment_uuid = e.uuid) as number_of_checkins
+FROM experiment e
+
+-- Experiments list
+SELECT e.uuid, e.name, c.email
 FROM experiment e
 INNER JOIN experiments_collaborators ec
     ON ec.experiment_uuid = e.uuid
 INNER JOIN collaborator c
     ON ec.collaborator_uuid = c.uuid
+
