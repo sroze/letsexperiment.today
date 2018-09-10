71ec3f533499ab4876f6a69772ef8394bc1b26cb
Samuel ROZE <samuel.roze@gmail.com>
2018-09-10 14:44:51 +0200

    Add the "ongoing experiments" count query

--- reporting/queries.sql
+++ reporting/queries.sql
@@ -15,3 +15,8 @@ INNER JOIN experiments_collaborators ec
 INNER JOIN collaborator c
     ON ec.collaborator_uuid = c.uuid
 
+-- On-going experiments
+SELECT COUNT(*)
+FROM experiment e
+WHERE period_end IS NOT NULL AND period_end > NOW()
+
